fb6d1ff2647f9251f402455e7091a0fbd966575f
Susan <susanbot@gmail.com>
2025-05-08 00:13:24 +0100

    feat: Plot histograms of iris features to one png file

--- analysis.py
+++ analysis.py
@@ -8,12 +8,18 @@
 import sys
 # Library for data analysis
 import pandas as pd
+# Libraries for plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # variable to hold Iris data filename
 INPUT_FILENAME = "bezdekiris.data"
 
 # variable to hold filename of text file to contain a summary of each feature
 OUTPUT_FILENAME = "iris_stats.txt"
+
+# List of features (columns) in the data
+feature_names = ["sepal length", "sepal width", "petal length", "petal width"]
 
 # open data file and read into dataframe
 with open (INPUT_FILENAME, 'rt') as iris_file:
@@ -22,12 +28,8 @@ with open (INPUT_FILENAME, 'rt') as iris_file:
     # Add column names manually as they are not present in the iris.data file.
     iris = pd.read_csv(
         iris_file, 
-        names=[
-            "sepal_length", 
-            "sepal_width", 
-            "petal_length", 
-            "petal_width", 
-            "species"])
+        names=feature_names + ["species"]
+        )
     
 
 # print out dataframe statistics per feature, overwrite file if exists
@@ -35,3 +37,50 @@ with open (OUTPUT_FILENAME, 'w+t') as stats_file:
     print(iris.describe(), file=stats_file)
 
 
+# Use Pyplot to create multiplot histogram of each feature in iris data
+fig, ax = plt.subplots(2, 2, layout ='constrained', figsize=(8, 6)) 
+# Create variable to iterate over the subplots
+this_subplot = 0
+
+# For each feature, by name
+for feature in feature_names:
+
+    # set which subplot we are working on, counting from 1
+    this_subplot += 1    
+    plt.subplot(2,2,this_subplot)
+    
+    # set number of bins 
+    nbins = 25
+
+    # Only plot the legend on the last subplot
+    if this_subplot == 4:
+        plot_legend_flag = True
+    else:
+        plot_legend_flag = False
+
+    # Use Seaborn to plot histograms
+    # ref: https://seaborn.pydata.org/generated/seaborn.histplot.html
+    # As data for all targets is called in one plot command, the histogram
+    # bins are the same across all three targets
+    sns.histplot(
+        iris, 
+        x=feature,
+        bins=nbins,
+        hue='species',
+        hue_order=["Iris-setosa","Iris-versicolor","Iris-virginica"],
+        alpha=0.5,
+        edgecolor='black',
+        legend=plot_legend_flag
+        )
+
+    # Set the x-axis label to the feature name, plus units
+    plt.xlabel(feature + " (cm)")
+
+    # Set the y-axis label to frequency in the dataset
+    plt.ylabel("Frequency")
+
+# Set overall title
+fig.suptitle("Histograms of features in the Iris Dataset")
+
+# Print to file
+plt.savefig('histograms_of_iris_features.png')
